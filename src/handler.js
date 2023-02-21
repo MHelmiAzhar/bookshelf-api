edc51afa06aa6edb4cc428bb8d2bb08d75297433
MHelmiAzhar <helmiazhar311201@gmail.com>
2023-02-21 11:17:51 +0700

    first commit

---
+++ src/handler.js
@@ -0,0 +1,179 @@
+const { nanoid } = require("nanoid");
+const bookshelf = require("./books");
+
+const addBookHnadler = (req, h) => {
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+  } = req.payload;
+  if (name === undefined) {
+    const response = h.response({
+      status: "fail",
+      message: "Gagal menambahkan buku. Mohon isi nama buku",
+    });
+    response.code(400);
+    return response;
+  }
+  if (readPage > pageCount) {
+    const response = h.response({
+      status: "fail",
+      message:
+        "Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount",
+    });
+    response.code(400);
+    return response;
+  }
+  const id = nanoid(16);
+  const insertedAt = new Date().toISOString();
+  const updatedAt = insertedAt;
+  const finished = pageCount === readPage;
+
+  const newBook = {
+    id,
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    finished,
+    reading,
+    insertedAt,
+    updatedAt,
+  };
+
+  bookshelf.push(newBook);
+  const isSuccess = bookshelf.filter((book) => book.id === id).length > 0;
+
+  if (isSuccess) {
+    const response = h.response({
+      status: "success",
+      message: "Buku berhasil ditambahkan",
+      data: {
+        bookId: id,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+
+  const response = h.response({
+    status: "error",
+    message: "Buku gagal ditambahkan",
+  });
+  response.code(500);
+  return response;
+};
+const getBookHandler = (req, h) => ({
+  status: "success",
+  data: {
+    books: bookshelf.map((book) => ({
+      id: book.id,
+      name: book.name,
+      publisher: book.publisher,
+    })),
+  },
+});
+
+const getBookByIdHandler = (req, h) => {
+  const { id } = req.params;
+  const book = bookshelf.filter((n) => n.id === id)[0];
+  if (book !== undefined) {
+    return {
+      status: "success",
+      data: {
+        book,
+      },
+    };
+  }
+  const response = h.response({
+    status: "fail",
+    message: "Buku tidak ditemukan",
+  });
+  response.code(404);
+  return response;
+};
+const editBookByIdHandler = (req, h) => {
+  const { bookId } = req.params;
+  const { name, year, author, summary, publisher, pageCount, readPage } =
+    req.payload;
+  const updatedAt = new Date().toISOString();
+  const index = bookshelf.findIndex((n) => n.id === bookId);
+  if (name === undefined) {
+    const response = h.response({
+      status: "fail",
+      message: "Gagal memperbarui buku. Mohon isi nama buku",
+    });
+    response.code(400);
+    return response;
+  }
+  if (readPage > pageCount) {
+    const response = h.response({
+      status: "fail",
+      message:
+        "Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount",
+    });
+    response.code(400);
+    return response;
+  }
+  if (index !== -1) {
+    bookshelf[index] = {
+      ...bookshelf[index],
+      name,
+      year,
+      author,
+      summary,
+      publisher,
+      pageCount,
+      readPage,
+      updatedAt,
+    };
+    const response = h.response({
+      status: "success",
+      message: "Buku berhasil diperbarui",
+    });
+    response.code(200);
+    return response;
+  }
+  const response = h.response({
+    status: "fail",
+    message: "Gagal memperbarui buku. Id tidak ditemukan",
+  });
+  response.code(404);
+  return response;
+};
+
+const deleteBookByIdHandler = (req, h) => {
+  const { bookid } = req.params;
+  const index = bookshelf.findIndex((n) => n.bookid === bookid);
+  if (index !== -1) {
+    bookshelf.splice(index, 1);
+    const response = h.response({
+      status: "success",
+      message: "Buku berhasil dihapus",
+    });
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: "fail",
+    message: "Buku gagal dihapus. Id tidak ditemukan",
+  });
+  response.code(404);
+  return response;
+};
+module.exports = {
+  addBookHnadler,
+  getBookHandler,
+  getBookByIdHandler,
+  editBookByIdHandler,
+  deleteBookByIdHandler,
+};
---
+++ src/routes.js
@@ -0,0 +1,37 @@
+const {
+  addBookHnadler,
+  getBookHandler,
+  getBookByIdHandler,
+  editBookByIdHandler,
+  deleteBookByIdHandler,
+} = require("./handler");
+
+const routes = [
+  {
+    method: "POST",
+    path: "/books",
+    handler: addBookHnadler,
+  },
+  {
+    method: "GET",
+    path: "/books",
+    handler: getBookHandler,
+  },
+  {
+    method: "GET",
+    path: "/books/{id}",
+    handler: getBookByIdHandler,
+  },
+  {
+    method: "PUT",
+    path: "/books/{bookId}",
+    handler: editBookByIdHandler,
+  },
+  {
+    method: "DELETE",
+    path: "/books/{bookId}",
+    handler: deleteBookByIdHandler,
+  },
+];
+
+module.exports = routes;
